3fd16c70ddca5d5ed3225d3c767141bd526fc464
Joe simpson <joe@egmrc.com>
2026-03-03 12:40:31 -0700

    fix: streamline server URL resolution in next.config.js
    - Simplified `NEXT_PUBLIC_SERVER_URL` assignment by removing `VERCEL_PROJECT_PRODUCTION_URL` logic.
    - Ensured consistency and cleaner environment variable handling.

--- next.config.js
+++ next.config.js
@@ -2,9 +2,7 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 import redirects from './redirects.js'
 
-const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
-  ? `${process.env.NEXT_PUBLIC_SERVER_URL}`
-  : process.env.__NEXT_PRIVATE_ORIGIN || 'http://localhost:3000'
+const NEXT_PUBLIC_SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL;
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
