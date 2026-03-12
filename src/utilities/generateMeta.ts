51ce1c529d35ebd10b420c5c949d27f4c67b0e46
Joe <joseph.simpson@jsdesdev.com>
2026-03-12 15:46:33 -0600

    Update Open Graph image to "museum-facade-edited-png.webp" for updated branding

--- src/utilities/generateMeta.ts
+++ src/utilities/generateMeta.ts
@@ -8,7 +8,7 @@ import { getServerSideURL } from './getURL'
 const getImageURL = (image?: Media | Config['db']['defaultIDType'] | null) => {
   const serverUrl = getServerSideURL()
 
-  let url = serverUrl + '/website-template-OG.webp'
+  let url = serverUrl + '/museum-facade-edited-png.webp'
 
   if (image && typeof image === 'object' && 'url' in image) {
     const ogUrl = image.sizes?.og?.url
